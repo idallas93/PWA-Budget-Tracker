fe7cc6b40d5fa4e5b3ca49ceeea0b647f22b97ba
idallas93 <Dallas.Isaac1@gmail.com>
2020-11-26 10:24:44 -0500

    fixed port

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-var port_number = server.listen(process.env.PORT || 3000);
+var PORT = server.listen(process.env.PORT || 3000);
 
 const app = express();
 
@@ -25,4 +25,4 @@ app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
-});
+})
