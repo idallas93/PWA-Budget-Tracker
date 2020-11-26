c694a93a950019d901fb55d187d8d5045db85e44
idallas93 <Dallas.Isaac1@gmail.com>
2020-11-26 10:27:17 -0500

    fixed mongose

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-var port_number = server.listen(process.env.PORT || 3000);
+var PORT = server.listen(process.env.PORT || 3000);
 
 const app = express();
 
@@ -15,12 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
